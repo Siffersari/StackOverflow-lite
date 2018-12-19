8b3d895064db26cca77a61536fbf5af600422359
LeewelKarani <Leewelkarani@gmail.com>
2018-12-19 20:56:02 +0300

    [chore #162750825] Add test for login in users

--- app/tests/v1/test_users.py
+++ app/tests/v1/test_users.py
@@ -43,8 +43,16 @@ class TestUsers(unittest.TestCase):
             "email": "Daisy@gmail.com",
             "password": "D@1sies"
         }
+        self.wrongUname = {
+            "fName": "Dai",
+            "lName": "Flower",
+            "uname": "Dasy",
+            "email": "Daisy@gmail.com",
+            "password": "D@1sies"
+        }
 
     def register_user(self, path="api/v1/auth/signup", data={}):
+        """ Registers a new user with given data or default if not provided """
         if not data:
             data = self.data
 
@@ -53,6 +61,7 @@ class TestUsers(unittest.TestCase):
         return response
 
     def login_user(self, path="api/v1/auth/login", data={}):
+        """ Logs in a user """
         if not data:
             data = self.data
 
@@ -63,17 +72,28 @@ class TestUsers(unittest.TestCase):
         return response
 
     def test_register_users(self):
+        """ Tests for user registration cases and validation of user data """
         new_user = self.register_user()
         userEmail = self.register_user(data=self.wrongEmail)
         userPass = self.register_user(data=self.wrongPass)
         userBadName = self.register_user(data=self.wrongName)
+        userUname = self.register_user(data=self.wrongUname)
         self.assertEqual(new_user.status_code, 201)
         self.assertEqual(userEmail.status_code, 400)
         self.assertEqual(userBadName.status_code, 400)
         self.assertEqual(userPass.status_code, 400)
-        self.assertTrue(userEmail.json["err"])
-        self.assertTrue(userPass.json["err"])
-        self.assertTrue(userBadName.json["err"])
+        self.assertEqual(userUname.status_code, 400)
+        self.assertTrue(userEmail.json["Err"])
+        self.assertTrue(userPass.json["Err"])
+        self.assertTrue(userBadName.json["Err"])
+        self.assertTrue(userUname.json["Err"])
 
     def test_login_users(self):
+        """ Tests for user login cases and validation of data """
         self.assertEqual(self.login_user().status_code, 200)
+        self.assertEqual(self.login_user(
+            data=self.wrongUname).status_code, 400)
+        self.assertEqual(self.login_user(data=self.wrongPass).status_code, 400)
+        self.assertTrue(self.login_user(data=self.wrongPass).json["Err"])
+        self.assertTrue(self.login_user(data=self.wrongUname).json["Err"])
+        self.assertTrue(self.login_user().json["Success"])
