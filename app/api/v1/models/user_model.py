d41009496aa84f6183d14ab82d6e426ffdfbe9d7
LeewelKarani <Leewelkarani@gmail.com>
2018-12-25 15:22:36 +0300

    [finishes #162834924] remove duplication

--- app/api/v1/models/user_model.py
+++ app/api/v1/models/user_model.py
@@ -30,21 +30,25 @@ class UserModels(object):
     def fetch_users(self):
         if not users:
             return jsonify({"Err": "There no registered users yet"}), 404
-        return self.db
+
+        return jsonify(self.db), 200
 
     def login_user(self, username, password):
-        passMatch, unameMatch = False, False
+        passMatch, unameMatch, position = False, False, 0
         for user in range(len(self.db)):
             for key, value in self.db[user].items():
                 if key == "uname":
                     if value == username:
                         unameMatch = True
-                        if self.db[user]["password"] == password:
-                            passMatch = True
-                            break
+                        position = user
+                        break
+
+        if self.db[position]["password"] == password:
+            passMatch = True
 
         if not unameMatch:
             return jsonify({"Err": "Please check your username"}), 400
+
         if not passMatch:
             return jsonify({"Err": "Please check your password"}), 400
 
@@ -57,19 +61,16 @@ class UserModels(object):
             if not value:
                 return jsonify({"Err": "{} is a required field.".format(key)}), 400
 
-            elif key == 'email':
-                if not re.search(r'\w+[.|\w]\w+@\w+[.]\w+[.|\w+]\w+', value):
-                    return jsonify({"Err": "Please enter a valid {}.".format(key)}), 400
+            if (key == 'email' and not re.search(r'\w+[.|\w]\w+@\w+[.]\w+[.|\w+]\w+', value)):
+                return jsonify({"Err": "Please enter a valid {}.".format(key)}), 400
 
-            elif key == 'fName' or key == 'lName' or key == 'uname':
-                if (len(value) < 4 or len(value) > 15):
-                    return jsonify({"Err": "{} should be 4-15 characters long".format(key)}), 400
+            if (key == 'fName' or key == 'lName' or key == 'uname') and (len(value) < 4 or len(value) > 15):
+                return jsonify({"Err": "{} should be 4-15 characters long".format(key)}), 400
 
-            elif key == 'password':
-                upper, lower = len(re.findall(
-                    r'[A-Z]', value)), len(re.findall(r'[a-z]', value))
-                digit, special = len(re.findall(
-                    r'[0-9]', value)), len(re.findall(r'[@#$]', value))
+            if key == 'password':
+                upper, lower, digit, special = len(re.findall(r'[A-Z]', value)), len(re.findall(
+                    r'[a-z]', value)), len(re.findall(r'[0-9]', value)), len(re.findall(r'[@#$]', value))
+
                 if not (upper and lower and digit and special):
                     return jsonify({"Err": "{} should contain atleast one number, uppercase, lowercase and special character".format(key)}), 400
 
